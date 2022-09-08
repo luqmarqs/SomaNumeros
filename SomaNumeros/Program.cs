5fe32e62227d8fe112aa030eda7ece03b0a8a005
luqmarqs <luq.marqs@gmail.com>
2022-09-08 10:36:16 -0300

    Adicionar arquivos de projeto.

---
+++ SomaNumeros/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite um número natural: ");
+
+int n = int.Parse(Console.ReadLine());
+
+int soma = 0;
+
+for(int i = 0; i <= n; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+        soma += i;
+}
+
+Console.WriteLine($"A soma dos múltiplos de 3 e 5 é {soma}");
